dba0d3f8dbd1519f8bc56384ec361902593e4eac
Guilherme <guilhermegalli5@gmail.com>
2021-04-06 23:13:05 -0300

    refactor and create route to get track object

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
-import routes from './routes';
+import TrackController from './controllers/Track';
+
+const trackController = new TrackController();
 
 const app = express();
 const port = 3333;
@@ -8,6 +10,6 @@ const port = 3333;
 app.use(cors());
 app.use(express.json());
 
-app.use(routes);
+app.get('/track/:trackCode', trackController.track);
 
 app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
