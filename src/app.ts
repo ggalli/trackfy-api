2487de4914b1dba857f1b89edea287c322e52f3f
Guilherme <guilhermegalli5@gmail.com>
2021-04-09 22:04:39 -0300

    up

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,15 @@
 import express from 'express';
-import cors from 'cors';
 import TrackController from './controllers/Track';
 
 const trackController = new TrackController();
 
 const app = express();
-const port = 3333;
+const port = process.env.PORT || 3333;
 
-app.use(cors());
 app.use(express.json());
 
 app.get('/track/:trackCode', trackController.track);
 
 app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
 
-module.exports = trackController.track;
+export default app;
